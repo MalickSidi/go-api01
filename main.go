05cfb80021435a79653cfd00a1eeade40518e8fb
Malick Mohamed <malick.s.mohamed@gmail.com>
2023-09-15 01:34:38 +0300

    test if setup is fine with net/http

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+  "net/http"
+  "fmt"
+)
 
 func main() {
-  fmt.Println("it's working?!")
+  server := &http.Server {
+
+    Addr: ":3000",
+    Handler: http.HandlerFunc(basicHandler),
+  }
+  err := server.ListenAndServe()
+  if err != nil {
+    fmt.Println("faild to listen to server", err)
+  }
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("Testing http, everything is alright?"))
 }
